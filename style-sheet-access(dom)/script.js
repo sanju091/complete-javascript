6635d5d5d66ed2cc98a35b698afd928e697b4463
sanjay choudhary <sanjaychoudhary002005@gmail.com>
2025-06-24 09:58:46 +0530

    Update script.js

---
+++ style-sheet-access(dom)/script.js
@@ -0,0 +1,10 @@
+// ----------------style sheet access in js-------------
+
+ 
+
+
+let div=document.querySelector("div");
+console.log(div);
+div.style.backgroundColor="green";
+div.style.fontSize="25px";
+div.style.visibility="hidden";
