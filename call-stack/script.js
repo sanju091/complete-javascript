fd73e86e61233c936ebdb70402552ee90049e75d
sanjay choudhary <sanjaychoudhary002005@gmail.com>
2025-06-24 09:52:13 +0530

    Update script.js

---
+++ call-stack/script.js
@@ -0,0 +1,12 @@
+/*------>>>>callStacks----<<<<<<<The call stack is a data structure that JavaScript uses to keep track of function calls.---
+Think of it like a stack of books 📚 — last one on, first one off (LIFO: Last In, First Out).*/
+
+
+
+function greeting(){
+      sayHi();
+  
+} function sayHi(){
+      console.log("hii..!");
+   }
+greeting();
