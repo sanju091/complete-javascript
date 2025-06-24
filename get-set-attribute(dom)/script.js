4187e143b25a187c363057527de4ab2e713a785c
sanjay choudhary <sanjaychoudhary002005@gmail.com>
2025-06-24 09:55:32 +0530

    Update script.js

---
+++ get-set-attribute(dom)/script.js
@@ -0,0 +1,12 @@
+// ------get and set attributes-------
+
+let div=document.querySelector("div");
+console.log(div);
+let id=div.getAttribute("id");
+console.log("id");
+
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+let para=document.querySelector("p");
+console.log(para.setAttribute("class","newClass"));
